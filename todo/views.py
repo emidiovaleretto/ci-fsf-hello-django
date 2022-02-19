ae82b47a45c627418cf220a09745cc2e8b8241da
Emidio Valeretto <emidio.valereto@gmail.com>
2022-02-19 22:25:50 +0000

    Create Item class and register Item Model.

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('<h1>Hello, World!</h1>')
+    return render(request, 'todo/todo_list.html')
