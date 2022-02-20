11c35790355d4bfb307559d123b8d9f318e88cd9
Emidio Valeretto <emidio.valereto@gmail.com>
2022-02-20 13:33:37 +0000

    Create a template to add items to database and add to urls.py.

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_todo_list, name='get_todo_list')
+    path('', views.get_todo_list, name='get_todo_list'),
+    path('add', views.add_new_item, name='add'),
 ]
