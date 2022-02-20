b04c76a0ed3bfdbc2881a218b8ff4c011424e882
Emidio Valeretto <emidio.valereto@gmail.com>
2022-02-20 14:44:47 +0000

    Create function to add new item to database and display it to the user.

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 def get_todo_list(request):
@@ -7,3 +7,14 @@ def get_todo_list(request):
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
+
+def add_new_item(request):
+
+    if request.method == 'POST':
+        item_name = request.POST.get('item-name')
+        done = 'is-done' in request.POST
+        Item.objects.create(name=item_name, done=done)
+
+        return redirect('get_todo_list')
+
+    return render(request, 'todo/add_item.html')
