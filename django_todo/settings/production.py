2788332aa26df027a8b864accbc1ddc1bfb705d9
Emidio Valeretto <emidio.valereto@gmail.com>
2022-02-22 10:07:47 +0000

    Add host to ALLOWED_HOSTS variable.

--- django_todo/settings/production.py
+++ django_todo/settings/production.py
@@ -7,12 +7,12 @@ if os.path.exists('env.py'):
 
 DEBUG = os.environ.get("DEBUG")
 SECRET_KEY = os.environ.get("SECRET_KEY_PRODUCTION")
-HOST = os.environ.get("HOST")
-POSTGRES_DB = os.environ.get("POSTGRES_DB")
 
 # ALLOWED_HOSTS
-ALLOWED_HOSTS=[HOST]
+ALLOWED_HOSTS=[
+    os.environ.get("HEROKU_HOST")
+]
 
 DATABASES = {
-    'default': dj_database_url.parse(POSTGRES_DB)
+    'default': dj_database_url.parse(os.environ.get("HEROKU_DB"))
 }
