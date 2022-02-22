4756db168baadf04d5a6b6c6fb120a0afbc8400a
Emidio Valeretto <emidio.valereto@gmail.com>
2022-02-22 01:35:30 +0000

    Fix host error.

--- django_todo/settings/production.py
+++ django_todo/settings/production.py
@@ -4,10 +4,11 @@ import dj_database_url
 
 DEBUG = config("DEBUG")
 SECRET_KEY = config("SECRET_KEY_PRODUCTION")
+HOST = config("HOST")
 POSTGRES_DB = config("POSTGRES_DB")
 
 # ALLOWED_HOSTS
-ALLOWED_HOSTS=['localhost', '127.0.0.1']
+ALLOWED_HOSTS=[HOST]
 
 DATABASES = {
     'default': dj_database_url.parse(POSTGRES_DB)
