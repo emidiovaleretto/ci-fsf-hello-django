9a93c8b44a2ed61a10ab37698f1dc65450627c71
Emidio Valeretto <emidio.valereto@gmail.com>
2022-02-22 01:44:19 +0000

    Config SECRET_KEY as envvar.

--- django_todo/settings/production.py
+++ django_todo/settings/production.py
@@ -1,11 +1,14 @@
+import os
 from .settings import *
-from decouple import config
 import dj_database_url
 
-DEBUG = config("DEBUG")
-SECRET_KEY = config("SECRET_KEY_PRODUCTION")
-HOST = config("HOST")
-POSTGRES_DB = config("POSTGRES_DB")
+if os.path.exists('env.py'):
+    import env
+
+DEBUG = os.environ.get("DEBUG")
+SECRET_KEY = os.environ.get("SECRET_KEY_PRODUCTION")
+HOST = os.environ.get("HOST")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
 
 # ALLOWED_HOSTS
 ALLOWED_HOSTS=[HOST]
